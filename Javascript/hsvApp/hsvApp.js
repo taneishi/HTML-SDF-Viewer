983e0af0fb4c93a136060bc83b7b622cc9fe5506
taneishi <taneishi@users.noreply.github.com>
2021-03-11 14:01:08 +0900

    remove angular app and css.

--- Javascript/hsvApp/hsvApp.js
+++
@@ -1,7 +0,0 @@
-//
-// Main app module file for hsvApp
-// Copyright David Cogrove, CozChemIx Limited 2017
-//  david@cozchemix.co.uk
-// Distributed under the BSD license, contained in the file license.txt in
-// the same directory as this file.
-var hsvApp = angular.module("hsvApp", ['fixed.table.header']);
