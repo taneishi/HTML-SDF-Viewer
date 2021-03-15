a3fe6eb6b59b18837be49cd35277281e15d76a13
taneishi <taneishi@users.noreply.github.com>
2021-03-15 13:09:19 +0900

    remote django dependent files.

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('data/', views.data, name='data'),
-    path('fingerprint/', views.fingerprint, name='fingerprint'),
-]
